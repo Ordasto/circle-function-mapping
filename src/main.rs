0a4d27581b1895dc579e7417fe389d7e0b1b03f8
Sam Peters <sam.peters24@gmail.com>
2023-11-14 16:46:30 +0000

    GUI with widgets, it feels like im doing something wrong with the GUI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::{f32::consts::PI, fmt::format};
+use macroquad::{prelude::*, ui::{widgets::{self, Group}, root_ui}};
 
-use macroquad::{miniquad::window::screen_size, prelude::*, rand, window};
-
-
+// use egui_macroquad;
 // This might be a bad idea, who knows
 type Theta = f32;
 type Line = (Theta, Theta);
@@ -14,26 +12,28 @@ async fn main() {
     let h = 1080.0 * 1.;
     set_fullscreen(true);
 
-    // request_new_screen_size(w, h);
-
-    let radius = 350.0;
+    let circle_size = 0.9; // percent of screen to fill,
+    let radius  = (h * circle_size) / 2.0; // fit to height,
 
     let center = Vec2 {
         x: w / 2.0,
         y: h / 2.0,
     };
-    
+
     let mut thet = 0.0;
 
-    let mut lines:Vec<Line> = (0..100).map(|x|
-        {
+    let mut lines: Vec<Line> = (0..1000)
+        .map(|_| {
             thet += 0.05;
-            return (thet, thet*1.2);
-        }
-    ).collect();
-    let mut running = false;
+            return (thet, thet);
+        })
+        .collect();
+    let mut running = true;
     let mut modif = 0.0;
+    let mut drawing_gui = true;
+
     loop {
+
 
         clear_background(BLACK);
         // Draw_circle kinda blocky
@@ -41,52 +41,70 @@ async fn main() {
 
         // recalc
         // p1.x = center.x + radius * theta.cos();
-        // p1.y = center.y + radius * theta.sin();  
+        // p1.y = center.y + radius * theta.sin();
         // p2.x = center.x + radius * theta.cos();
         // p2.y = center.y + radius * theta.sin();
 
-        if running {
-            for (theta1,theta2) in lines.iter_mut() {
-                // *theta1 += 0.00;
-                // at some point, make the theta calc a function pointer or something
-                // so that i can pass in different functions faster, takes in a f32 spits out f32 easy
-                // ALSO make a gui to modify the parameters, imgui stype (immediate gui)
-
-                *theta2 = *theta1 * modif;
+        for (theta1, theta2) in lines.iter_mut() {
+            // *theta1 += 0.00;
+            // at some point, make the theta calc a function pointer or something
+            // so that i can pass in different functions faster
+            // ALSO make a gui to modify the parameters, imgui stype (immediate gui)
+            if running {
+                // do custom theta_function  fn theta_function(f32, f32) -> (f32, f32)
+                // variable that stores currently selected theta function, function to set it
+                *theta2 = *theta1 + modif;
                 modif += 0.00005;
-
-                let x1 = center.x + radius * theta1.cos();
-                let y1 = center.y + radius * theta1.sin();  
-                let x2 = center.x + radius * theta2.cos();
-                let y2 = center.y + radius * theta2.sin();
-
-                draw_line(x1, y1, x2, y2, 2.0, WHITE);
             }
+
+            let x1 = center.x + radius * theta1.cos();
+            let y1 = center.y + radius * theta1.sin();
+            let x2 = center.x + radius * theta2.cos();
+            let y2 = center.y + radius * theta2.sin();
+
+            draw_line(x1, y1, x2, y2, 0.9, WHITE);
         }
-        if is_key_pressed(KeyCode::Space){
+
+        // [DO THIS BEFORE IT'S TOO GROSS]
+        // i hate this current system, use callbacks or some shit
+        // have a hash list of key:value pairs, new keybinding is appending a key:KeyCode and value:Vec<functions>
+        // if keybind already exsists, add function to vec, else new pair
+
+        if is_key_pressed(KeyCode::Space) {
             running = !running;
         }
-        if is_key_down(KeyCode::Escape){
+        if is_key_pressed(KeyCode::E) { 
+            drawing_gui = !drawing_gui;
+        }
+        if is_key_down(KeyCode::Escape) {
             break;
+        }
+
+        
+        if drawing_gui { // code  put this inside the function with a paramter to clean it up
+            draw_gui();
         }
 
         next_frame().await;
     }
-
-
-
 }
 
+fn draw_gui(){
+    widgets::Window::new(21, vec2(50., 50.), vec2(200., 200.))
+    .label("Settings")
+    .movable(true)
+    .titlebar(true)
+    .ui(&mut root_ui(), |ui|{
+        ui.label(vec2(2.0, 5.0), "1");
+        ui.label(vec2(2.0, 15.0), "2");
 
-fn draw_linev(p1: Vec2, p2: Vec2) {
-    draw_line(p1.x, p1.y, p2.x, p2.y, 2.0, WHITE);
-
+    });
 }
 
-
-
-// fn draw_line_theta()
-
-// cool
+// Pulsing constrictiong thing
 // *theta2 = *theta1 + modif;
+// modif += 0.00005;
+
+// cool peaks forming thing
+// *theta2 = *theta1 * modif;
 // modif += 0.00005;
